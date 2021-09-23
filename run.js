6f47231ed691678b4f3b9d8fcc265ad2f1f86f1f
Jan Mrowiec <jan.mrowiec@footballco.com>
2021-09-23 18:59:01 +0200

    add js script with selectors

---
+++ run.js
@@ -0,0 +1,3 @@
+document.querySelector('form').addEventListener('submit', handleSubmit);
+document.querySelector('ul').addEventListener('click', handleDoneOrRemove);
+document.getElementById('clear').addEventListener('click', handleClear);
