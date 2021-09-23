5d059eb459065b06b99d479222e65230bb0480c8
Jan Mrowiec <jan.mrowiec@footballco.com>
2021-09-23 23:57:12 +0200

    add js function to handle adding tasks

--- run.js
+++ run.js
@@ -1,3 +1,21 @@
 document.querySelector('form').addEventListener('submit', handleSubmit);
-document.querySelector('ul').addEventListener('click', handleDoneOrRemove);
-document.getElementById('clear').addEventListener('click', handleClear);
+// document.querySelector('ul').addEventListener('click', handleDoneOrRemove);
+// document.getElementById('clear').addEventListener('click', handleClear);
+
+function handleSubmit(element) {
+	element.preventDefault();
+	let input = document.querySelector('input');
+	if (input.value != '')
+		addTask(input.value);
+}
+
+function addTask(task) {
+	let form = document.createElement('form');
+
+	form.innerHTML = `
+        <input type="text" value="${task}">
+    `;
+
+    document.querySelector('input').value = ''
+    document.getElementById('list').appendChild(form);
+}
