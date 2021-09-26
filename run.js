46c8bd24210d96db48fa8dc4833315fc4a77f76f
Jan Mrowiec <jan.mrowiec@footballco.com>
2021-09-26 11:23:07 +0200

    refactor main script

--- run.js
+++ run.js
@@ -1,41 +1,49 @@
 // selectors
-
-document.querySelector('form').addEventListener('submit', handleSubmit);
+document.querySelector('form').addEventListener('submit', handleAddNewHorse);
 document.querySelector('ul').addEventListener('click', handleFinishOrRemove);
-document.getElementById('clear').addEventListener('click', handleClearList);
+document.getElementById('clear').addEventListener('click', handleRemoveAll);
 
 
 // event handlers 
-
-function handleSubmit(element) {
-	element.preventDefault();
+function handleAddNewHorse(e) {
+	e.preventDefault();
 	let input = document.querySelector('input');
 	if (input.value != '')
 		addTask(input.value);
+	input.value = ''
 }
 
 
-function handleClearList(e) {
+function handleFinishOrRemove(e) {
+	if (e.target.name == 'doneButton')
+		finishTask(e);
+	if (e.target.name == 'removeButton')
+		addTask(e);
+}
+
+
+function handleRemoveAll(e) {
 	document.querySelector('ul').innerHTML = '';
 }
-
-function handleFinishOrRemove(argument) {
-	// body...
-}
-
 
 
 // helpers
 
 function addTask(task) {
-	let form = document.createElement('form');
+	let ul = document.querySelector('ul');
+	let li = document.createElement('li');
 
-	form.innerHTML = `
-        <input type="text" value="${task}">
-    `;
-
-    document.querySelector('input').value = ''
-    document.getElementById('list').appendChild(form);
+	li.innerHTML = `
+		<span class="item-title">${task}</span>
+		<button name="doneButton">
+			<img src="https://img.icons8.com/ios/50/000000/checked--v1.png"/>
+		</button>
+		<button name="removeButton">
+        	<img src="https://img.icons8.com/ios/50/000000/trash--v1.png"/>
+        </button>
+	`;
+	li.classList.add('list-item');
+	ul.appendChild(li);
 }
 
 function finishTask(argument) {
